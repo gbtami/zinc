7ecf8c8963625f284efb1ac59d5898013c8081be
lucasart <lucas.braesch@gmail.com>
2016-06-19 11:27:34 +0800

    Do not use chess.uci module Instead, I wrote my own UCI class, with minimalistic and tailor made features. I found chess.uci too complicated to use, because of all this sub-classing, and event handling async stuff. No async I/O is needed here (except to check for timeout, will do later).

--- zinc.py
+++ zinc.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-import os, multiprocessing, time
+import os, subprocess, multiprocessing, time
 import math, statistics
-import chess, chess.uci
+import chess
 
 # Parameters
 Engines = [
@@ -9,8 +9,8 @@ Engines = [
     {'file': '../Stockfish/base', 'name' : 'base'}
 ]
 Options = [
-    {'Hash': 16, 'Contempt': 10},
-    {'Hash': 16, 'Contempt': 10}
+    {'Hash': 16, 'Threads': 1},
+    {'Hash': 16, 'Threads': 1}
 ]
 TimeControl = {'depth': None, 'nodes': None, 'movetime': None, 'time': 2, 'inc': 0.02}
 Draw = {'movenumber': 40, 'movecount': 8, 'score': 20}
@@ -19,19 +19,87 @@ Openings = '../book5.epd'
 Games = 50
 Concurrency = 7
 
+class UCI(object):
+    def __init__(self, cmd, name):
+        self.process = subprocess.Popen(cmd, stdout=subprocess.PIPE, stdin=subprocess.PIPE, universal_newlines=True)
+        self.name = name
+        self.options = []
+
+    def readline(self):
+        line = self.process.stdout.readline()
+        line = line[:-1]
+        # print('%s > %s' % (self.name, line))
+        return line
+
+    def writeline(self, string):
+        # print('%s < %s' % (self.name, string))
+        self.process.stdin.write(string)
+        self.process.stdin.write('\n')
+        self.process.stdin.flush()
+
+    def uci(self):
+        self.writeline('uci')
+        while True:
+            line = self.readline()
+            if line.startswith('option name'):
+                name = ''
+                tokens = line.split()
+                for i in range(2, len(tokens)):
+                    if tokens[i] == 'type':
+                        break
+                    name += tokens[i] + ' '
+                self.options.append(name[:-1])
+            elif line == 'uciok':
+                break
+
+    def setoption(self, options):
+        for name in options:
+            self.writeline('setoption name {} value {}'.format(name, options[name]))
+
+    def isready(self):
+        self.writeline('isready')
+        while self.readline() != 'readyok':
+            pass
+
+    def go(self, args):
+        tokens = ['go']
+        for name in args:
+            if args[name]:
+                tokens += [name, str(args[name])]
+        self.writeline(' '.join(tokens))
+
+        score = None
+        while True:
+            line = self.readline()
+            if line.startswith('info'):
+                subStr = 'score cp '
+                i = line.find(subStr)
+                if i != -1:
+                    tokens = line[(i + len(subStr)):].split()
+                    if ('lowerbound' not in tokens) and ('upperbound' not in tokens):
+                        score = int(tokens[0])
+            elif line.startswith('bestmove'):
+                return line.split()[1], score
+
+    def quit(self):
+        self.writeline('quit')
+        self.process.wait()
+
 def start_engine(i):
-    e = chess.uci.popen_engine(Engines[i]['file'])
+    e = UCI(Engines[i]['file'], Engines[i]['name'])
     e.uci()
-    e.name = Engines[i]['name']
+
     for name in Options[i]:
         if name not in e.options:
-            print('warning: "%s" is not a valid UCI Option for engine "%s"'
-                % (name, e.name))
+            print('warning: "%s" is not a valid UCI Option for engine "%s"' % (name, e.name))
+
     e.setoption(Options[i])
     e.isready()
-    e.ucinewgame()
-    e.info_handlers.append(chess.uci.InfoHandler())
+    e.writeline('ucinewgame')
     return e
+
+def to_msec(sec):
+    return int(sec * 1000)
 
 def play(game):
     # Start engines and clocks
@@ -42,34 +110,40 @@ def play(game):
 
     # Setup the position, and determine which engine plays first
     board = chess.Board(game['fen'])
+    uciMoves = []
     i = game['white'] ^ (board.turn == chess.BLACK)
 
     # Play the game
     drawCnt, resignCnt = 0, 0 # in plies
     lostOnTime = None
+
     while (not board.is_game_over(True)):
-        engines[i].position(board)
+        posCmd = 'position fen ' + game['fen']
+        if uciMoves:
+            posCmd += ' moves ' + ' '.join(uciMoves)
+        engines[i].writeline(posCmd)
         engines[i].isready()
 
         startTime = time.time()
-        bestmove, ponder = engines[i].go(
-            depth = TimeControl['depth'],
-            nodes = TimeControl['nodes'],
-            movetime = TimeControl['movetime'],
-            wtime = int(clocks[game['white']] * 1000),
-            btime = int(clocks[game['white'] ^ 1] * 1000),
-            winc = TimeControl['inc'],
-            binc = TimeControl['inc']
-        )
+        bestmove, score = engines[i].go({
+            'depth': TimeControl['depth'],
+            'nodes': TimeControl['nodes'],
+            'movetime': TimeControl['movetime'],
+            'wtime': to_msec(clocks[game['white']]),
+            'btime': to_msec(clocks[game['white'] ^ 1]),
+            'winc': to_msec(TimeControl['inc']),
+            'binc': to_msec(TimeControl['inc'])
+        })
         elapsed = time.time() - startTime
 
         clocks[i] -= elapsed
+
         if (clocks[i] < 0):
             lostOnTime = i
             break
+
         clocks[i] += TimeControl['inc']
 
-        score = engines[i].info_handlers[0].info['score'][1].cp
         if score != None:
             # Resign adjudication
             if abs(score) >= Resign['score']:
@@ -90,7 +164,8 @@ def play(game):
             # Disable adjudication over mate scores
             drawCnt, resignCnt = 0, 0
 
-        board.push(bestmove)
+        board.push_uci(bestmove)
+        uciMoves.append(bestmove)
         i ^= 1
 
     result, reason = board.result(True), 'chess rules'
@@ -101,14 +176,14 @@ def play(game):
             result = '1-0' if lostOnTime == game['white'] else '0-1'
             reason = 'lost on time'
         elif resignCnt >= 2 * Resign['movecount']:
-            reason = 'resigned'
+            reason = 'adjudication'
             if score > 0:
                 result = '1-0' if board.turn == chess.WHITE else '0-1'
             else:
                 result = '0-1' if board.turn == chess.WHITE else '1-0'
         else:
             result = '1/2-1/2'
-            reason = 'drawn by adjudication'
+            reason = 'adjudication'
 
     # Display results
     print('Game #%d: %s vs. %s: %s (%s)' % (game['idx'] + 1, engines[game['white']].name,
@@ -143,7 +218,8 @@ results = pool.map(play, games)
 pool.close()
 pool.join()
 
-# Print statistics
-score = statistics.mean(results)
-margin = 1.96 * statistics.stdev(results) / math.sqrt(Games)
-print('score = %.2f%% +/- %.2f%%' % (100 * score, 100 * margin))
+if Games >= 2:
+    # Print statistics
+    score = statistics.mean(results)
+    margin = 1.96 * statistics.stdev(results) / math.sqrt(Games)
+    print('score = %.2f%% +/- %.2f%%' % (100 * score, 100 * margin))
